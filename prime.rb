791c03a216bf7110bbf1e4add4af15c866137af0
Russell Macalintal <russell.macalintal@gmail.com>
2019-10-06 03:46:40 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,10 @@
 # Add  code here!
+def prime?(number)
+  if number == 2
+    true
+  elsif number > 2 && (2..(number - 1)).none? {|divider| number%divider == 0}
+    true
+  else
+    false
+  end
+end
